efdf31f1e4f8036faa3c2e4746acd69ba6b15e8e
Laura <lburdic2@depaul.edu>
2025-02-18 08:34:54 -0600

    Dashboard page

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from UniVerse import settings
+from Housing.models import HousingListing, HousingBooking
+
 
 # Create your views here.
+
+def dashboard(request):
+    # listings = HousingListing.objects.all()
+    user=request.user
+
+    if not user.is_authenticated:
+        return render(request, 'Dashboard/dashboard.html')
+        
+    user_listings=HousingListing.objects.filter(user=user)
+
+    user_bookings=HousingBooking.objects.filter(user=user)
+
+    return render(request, 'Dashboard/dashboard.html', {'user_listings': user_listings, 'user_bookings': user_bookings})
+
+
+
--- UniVerse/urls.py
+++ UniVerse/urls.py
@@ -21,4 +21,5 @@ from Housing import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Housing.urls')),
+    path('', include('Dashboard.urls'))
 ]
