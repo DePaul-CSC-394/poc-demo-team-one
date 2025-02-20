84b284d7618d0499b15345e83761c132fa5d334e
James <juntiantong218@gmail.com>
2025-02-19 19:11:14 -0600

    add listing page

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard', views.dashboard, name='dashboard')
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('dashboard/add_listing', views.add_listing, name = 'add_listing')
 ]
