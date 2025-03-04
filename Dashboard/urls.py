7e7ab39bee8dd5130ea1042f91fdd7e0928e3985
Laura <lburdic2@depaul.edu>
2025-03-04 07:38:29 -0600

    Added tiles and suuply sections of dashboard

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('add_listing/', views.add_listing, name='add_listing'),
     path('add_supplies/', views.add_supplies, name='add_supplies'),
     path('approve_or_deny', views.approve_or_deny, name='approve_or_deny'),
-    path('delete-listing/<int:listing_id>/', views.delete_listing, name='delete_listing'),
-    path('delete-supplies/<int:supplies_id>/', views.delete_supplies, name='delete_supplies'),
+    path('delete-listing/<int:listing_id>/', views.delete_listing, name='delete_listing')
 ]
