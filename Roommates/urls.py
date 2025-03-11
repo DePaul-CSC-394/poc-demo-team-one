54d14c716a68193ef92ba712dc28d4fe5a279cfb
Andrew Kostryba <ankostryb@gmail.com>
2025-03-11 14:33:01 -0500

    roommate nav redirect, edit response btn, dashboard styling

--- Roommates/urls.py
+++ Roommates/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.roommates, name='roommates'),  
     path('roommatesDashboard/', views.roommatesDashboard, name='roommatesDashboard'),
     path("questionnaire/", views.questionnaire_view, name="questionnaire"),
+    path("edit-responses/", views.editResponses, name="editResponses")
 ]
